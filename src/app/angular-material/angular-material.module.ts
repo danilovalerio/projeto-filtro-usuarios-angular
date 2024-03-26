6bf6f7191e1558abd39c801d32e0194908a41a4a
danilovalerio <danilo_vs@hotmail.com>
2024-03-26 14:50:27 -0300

    User-details componente html

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
-  imports: [MatButtonModule, MatListModule],
-  exports: [MatButtonModule, MatListModule],
+  imports: [MatButtonModule, MatListModule, MatDividerModule],
+  exports: [MatButtonModule, MatListModule, MatDividerModule],
 })
 export class AngularMaterialModule {}
