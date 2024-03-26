edb4731d787ca17aa86e11bbd1730aac9f47f8a6
danilovalerio <danilo_vs@hotmail.com>
2024-03-26 11:47:09 -0300

    JSON Mock - Dados mockados para nossa aplicacao

---
+++ src/app/data/users-list.ts
@@ -0,0 +1,25 @@
+export const UsersList = [
+  {
+    nome: 'Joao da Silva',
+    email: 'joao.silva@exemplo.com',
+    senha: 'senha123',
+    idade: 30,
+    endereco: {
+      rua: 'Rua dos Sóis',
+      numero: 42,
+      cidade: 'São Paulo',
+      estado: 'SP',
+      pais: 'Brasil',
+    },
+    telefone: '11953601234',
+    ativo: true,
+    funcao: 'Desenvolvedor',
+    dataCadastro: '2023-08-01T09:00:00.000Z',
+    status: {
+      online: true,
+      verificado: true,
+      assinaturaAtiva: true,
+      ultimoAcesso: '2023-08-01T09:00:00.000Z',
+    },
+  },
+];
