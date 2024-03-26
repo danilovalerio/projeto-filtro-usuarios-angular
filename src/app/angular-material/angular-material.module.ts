b3d985486f75e5080f1373ec8188e8c789bc9725
danilovalerio <danilo_vs@hotmail.com>
2024-03-26 16:14:23 -0300

    Input - form name component

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -3,9 +3,23 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  imports: [MatButtonModule, MatListModule, MatDividerModule],
-  exports: [MatButtonModule, MatListModule, MatDividerModule],
+  imports: [
+    MatButtonModule,
+    MatListModule,
+    MatDividerModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatListModule,
+    MatDividerModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
 })
 export class AngularMaterialModule {}
--- src/app/componentes/components.module.ts
+++ src/app/componentes/components.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
+
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { FilterComponent } from './filter/filter.component';
 
@@ -10,7 +13,7 @@ import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [UserDetailsComponent, FilterComponent],
-  imports: [AngularMaterialModule],
+  imports: [FormsModule, AngularMaterialModule],
   exports: [UserDetailsComponent, FilterComponent],
 })
 export class ComponentsModule {}
