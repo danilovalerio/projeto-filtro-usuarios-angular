fd740f9457c93a85f81a7ca1a63f32edbd0ddf60
danilovalerio <danilo_vs@hotmail.com>
2024-03-26 10:01:46 -0300

    Angular Material - Exemplo de Imports e Exports dos módulos MatButtonModule e MatIconModule, mais uso dos mesmos no appcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './componentes/components.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AngularMaterialModule } from './angular-material/angular-material.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ComponentsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ComponentsModule,
+    AngularMaterialModule,
+  ],
   providers: [provideClientHydration(), provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
