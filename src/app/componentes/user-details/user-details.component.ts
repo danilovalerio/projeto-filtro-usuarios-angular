40325ed83b4bfd6beb32689a73e80c9233cb5fc9
danilovalerio <danilo_vs@hotmail.com>
2024-03-27 16:06:50 -0300

    UserDetails - interpolacao das propriedades

--- src/app/componentes/user-details/user-details.component.ts
+++ src/app/componentes/user-details/user-details.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IUser } from '../../interfaces/user/user.interface';
 
+//dumb component
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrl: './user-details.component.scss'
+  styleUrl: './user-details.component.scss',
 })
 export class UserDetailsComponent {
-
+  //Inicializando com um objeto vazio fazendo um casting, gerando um objeto vazio com undefined
+  @Input({ required: true }) user: IUser = {} as IUser;
 }
