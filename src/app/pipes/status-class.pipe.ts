dac99ce750f09f5888f36712e3035d49a85893b1
danilovalerio <danilo_vs@hotmail.com>
2024-04-03 15:21:13 -0300

    STATUS PIPE VALIDATION

--- src/app/pipes/status-class.pipe.ts
+++ src/app/pipes/status-class.pipe.ts
@@ -5,9 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class StatusClassPipe implements PipeTransform {
   transform(status: boolean): string {
+    const INVALID_STATUS = status === undefined || status === null;
+
     console.log('StatusClassPipe foi usada');
 
     //TODO: Extrair para constantsUtils o Ativo e Inativo
+    if (INVALID_STATUS) {
+      return 'Status inválido ou indisponível.';
+    }
+
     if (status) {
       return 'Ativo';
     } else {
