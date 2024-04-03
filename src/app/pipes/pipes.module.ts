260f2bcf5a658ba4749c889763df524f4f524ddd
danilovalerio <danilo_vs@hotmail.com>
2024-04-03 15:34:57 -0300

    Pipe dash if is empty

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -3,9 +3,16 @@ import { StatusClassPipe } from './status-class.pipe';
 import { PhonePipe } from './phone.pipe';
 import { AddressPipe } from './address.pipe';
 import { CommonModule } from '@angular/common';
+import { DashIfEmptyPipe } from './dash-if-empty.pipe';
 
 @NgModule({
-  declarations: [StatusClassPipe, PhonePipe, AddressPipe],
-  exports: [StatusClassPipe, PhonePipe, AddressPipe, CommonModule],
+  declarations: [StatusClassPipe, PhonePipe, AddressPipe, DashIfEmptyPipe],
+  exports: [
+    StatusClassPipe,
+    PhonePipe,
+    AddressPipe,
+    CommonModule,
+    DashIfEmptyPipe,
+  ],
 })
 export class PipesModulo {}
