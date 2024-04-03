10a7d7deaa379f1969640b37f5968c6d33711764
danilovalerio <danilo_vs@hotmail.com>
2024-04-03 15:29:08 -0300

    Date pipe que fica em comom module do próprio Angular

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { StatusClassPipe } from './status-class.pipe';
 import { PhonePipe } from './phone.pipe';
 import { AddressPipe } from './address.pipe';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [StatusClassPipe, PhonePipe, AddressPipe],
-  exports: [StatusClassPipe, PhonePipe, AddressPipe],
+  exports: [StatusClassPipe, PhonePipe, AddressPipe, CommonModule],
 })
 export class PipesModulo {}
